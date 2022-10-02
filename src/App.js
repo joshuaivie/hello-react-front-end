2e5b190f7630c3a182a9dbe90b2c1b9600685134
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-10-02 03:33:42 +0100

    setup greeting component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import Greeting from './Greeting';
+
+function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Greetings App!</h1>
+      <Link to="/greetings">Click here to see a greeting!</Link>
+    </main>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/greetings" element={<Greeting />} />
+    </Routes>
   );
 }
 
